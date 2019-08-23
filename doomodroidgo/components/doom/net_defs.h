b182b3bd3ca798833d5e7b665f4a0b3681a49185
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-08-23 20:04:43 +0200

    Make more senseable net parameters. Still .bss Segment full, don't know why...

--- doomodroidgo/components/doom/net_defs.h
+++ doomodroidgo/components/doom/net_defs.h
@@ -29,14 +29,14 @@
 // (eg. left/right monitors)
 
 //#define MAXNETNODES 16
-#define MAXNETNODES 2
+#define MAXNETNODES 8
 
 // The maximum number of players, multiplayer/networking.
 // This is the maximum supported by the networking code; individual games
 // have their own values for MAXPLAYERS that can be smaller.
 
 //#define NET_MAXPLAYERS 8
-#define NET_MAXPLAYERS 2
+#define NET_MAXPLAYERS 4
 
 // Maximum length of a player's name.
 
@@ -45,7 +45,7 @@
 // Networking and tick handling related.
 
 //#define BACKUPTICS 128
-#define BACKUPTICS 2 
+#define BACKUPTICS 32 
 
 typedef struct _net_module_s net_module_t;
 typedef struct _net_packet_s net_packet_t;
