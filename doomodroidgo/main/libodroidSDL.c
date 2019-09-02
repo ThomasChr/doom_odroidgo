a938edfddde5019e3252152b1bcfe0f5dbfa6475
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-09-02 08:10:22 +0200

    Wrong function name

--- doomodroidgo/main/libodroidSDL.c
+++ doomodroidgo/main/libodroidSDL.c
@@ -2,329 +2,329 @@
 
 int SDLCALL SDL_BuildAudioCVT(SDL_AudioCVT * cvt, SDL_AudioFormat src_format, Uint8 src_channels, int src_rate, SDL_AudioFormat dst_format, Uint8 dst_channels, int dst_rate)
 {
-    put("SDL_BuildAudioCVT");
+    puts("SDL_BuildAudioCVT");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_CondSignal(SDL_cond * cond)
 {
-    put("SDL_CondSignal");
+    puts("SDL_CondSignal");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_CondWait(SDL_cond * cond, SDL_mutex * mutex)
 {
-    put("SDL_CondWait");
+    puts("SDL_CondWait");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_ConvertAudio(SDL_AudioCVT * cvt)
 {
-    put("SDL_ConvertAudio");
+    puts("SDL_ConvertAudio");
     /* TODO */
     return 0;
 }
 
 SDL_cond *SDLCALL SDL_CreateCond(void)
 {
-    put("SDL_CreateCond");
+    puts("SDL_CreateCond");
     /* TODO */
     return 0;
 }
 
 SDL_mutex *SDLCALL SDL_CreateMutex(void)
 {
-    put("SDL_CreateMutex");
+    puts("SDL_CreateMutex");
     /* TODO */
     return 0;
 }
 
 SDL_Renderer * SDLCALL SDL_CreateRenderer(SDL_Window * window, int index, Uint32 flags)
 {
-    put("SDL_CreateRenderer");
+    puts("SDL_CreateRenderer");
     /* TODO */
     return 0;
 }
 
 SDL_Surface *SDLCALL SDL_CreateRGBSurface(Uint32 flags, int width, int height, int depth, Uint32 Rmask, Uint32 Gmask, Uint32 Bmask, Uint32 Amask)
 {
-    put("SDL_CreateRGBSurface");
+    puts("SDL_CreateRGBSurface");
     /* TODO */
     return 0;
 }
 
 SDL_Surface *SDLCALL SDL_CreateRGBSurfaceFrom(void *pixels, int width, int height, int depth, int pitch, Uint32 Rmask, Uint32 Gmask, Uint32 Bmask, Uint32 Amask)
 {
-    put("SDL_CreateRGBSurfaceFrom");
+    puts("SDL_CreateRGBSurfaceFrom");
     /* TODO */
     return 0;
 }
 
 SDL_Texture * SDLCALL SDL_CreateTexture(SDL_Renderer * renderer, Uint32 format, int access, int w, int h)
 {
-    put("SDL_CreateTexture");
+    puts("SDL_CreateTexture");
     /* TODO */
     return 0;
 }
 
 SDL_Texture * SDLCALL SDL_CreateTextureFromSurface(SDL_Renderer * renderer, SDL_Surface * surface)
 {
-    put("SDL_CreateTextureFromSurface");
+    puts("SDL_CreateTextureFromSurface");
     /* TODO */
     return 0;
 }
 
 SDL_Thread * SDLCALL SDL_CreateThread(SDL_ThreadFunction fn, const char *name, void *data)
 {
-    put("SDL_CreateThread");
+    puts("SDL_CreateThread");
     /* TODO */
     return 0;
 }
 
 SDL_Window * SDLCALL SDL_CreateWindow(const char *title, int x, int y, int w, int h, Uint32 flags)
 {
-    put("SDL_CreateWindow");
+    puts("SDL_CreateWindow");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_Delay(Uint32 ms)
 {
-    put("SDL_Delay");
+    puts("SDL_Delay");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_DestroyCond(SDL_cond * cond)
 {
-    put("SDL_DestroyCond");
+    puts("SDL_DestroyCond");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_DestroyMutex(SDL_mutex * mutex)
 {
-    put("SDL_DestroyMutex");
+    puts("SDL_DestroyMutex");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_DestroyRenderer(SDL_Renderer * renderer)
 {
-    put("SDL_DestroyRenderer");
+    puts("SDL_DestroyRenderer");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_DestroyTexture(SDL_Texture * texture)
 {
-    put("SDL_DestroyTexture");
+    puts("SDL_DestroyTexture");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_FillRect(SDL_Surface * dst, const SDL_Rect * rect, Uint32 color)
 {
-    put("SDL_FillRect");
+    puts("SDL_FillRect");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_free(void *mem)
 {
-    put("SDL_free");
+    puts("SDL_free");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_FreeSurface(SDL_Surface * surface)
 {
-    put("SDL_FreeSurface");
+    puts("SDL_FreeSurface");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_GetCurrentDisplayMode(int displayIndex, SDL_DisplayMode * mode)
 {
-    put("SDL_GetCurrentDisplayMode");
+    puts("SDL_GetCurrentDisplayMode");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_GetDisplayBounds(int displayIndex, SDL_Rect * rect)
 {
-    put("SDL_GetDisplayBounds");
+    puts("SDL_GetDisplayBounds");
     /* TODO */
     return 0;
 }
 
 const char *SDLCALL SDL_GetError(void)
 {
-    put("SDL_GetError");
+    puts("SDL_GetError");
     /* TODO */
     return 0;
 }
 
 SDL_Keycode SDLCALL SDL_GetKeyFromScancode(SDL_Scancode scancode)
 {
-    put("SDL_GetKeyFromScancode");
+    puts("SDL_GetKeyFromScancode");
     /* TODO */
     return 0;
 }
 
 const char *SDLCALL SDL_GetKeyName(SDL_Keycode key)
 {
-    put("SDL_GetKeyName");
+    puts("SDL_GetKeyName");
     /* TODO */
     return 0;
 }
 
 SDL_Keymod SDLCALL SDL_GetModState(void)
 {
-    put("SDL_GetModState");
+    puts("SDL_GetModState");
     /* TODO */
     return 0;
 }
 
 Uint32 SDLCALL SDL_GetMouseState(int *x, int *y)
 {
-    put("SDL_GetMouseState");
+    puts("SDL_GetMouseState");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_GetNumVideoDisplays(void)
 {
-    put("SDL_GetNumVideoDisplays");
+    puts("SDL_GetNumVideoDisplays");
     /* TODO */
     return 0;
 }
 
 char *SDLCALL SDL_GetPrefPath(const char *org, const char *app)
 {
-    put("SDL_GetPrefPath");
+    puts("SDL_GetPrefPath");
     /* TODO */
     return 0;
 }
 
 Uint32 SDLCALL SDL_GetRelativeMouseState(int *x, int *y)
 {
-    put("SDL_GetRelativeMouseState");
+    puts("SDL_GetRelativeMouseState");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_GetRendererInfo(SDL_Renderer * renderer, SDL_RendererInfo * info)
 {
-    put("SDL_GetRendererInfo");
+    puts("SDL_GetRendererInfo");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_GetRendererOutputSize(SDL_Renderer * renderer, int *w, int *h)
 {
-    put("SDL_GetRendererOutputSize");
+    puts("SDL_GetRendererOutputSize");
     /* TODO */
     return 0;
 }
 
 Uint32 SDLCALL SDL_GetTicks(void)
 {
-    put("SDL_GetTicks");
+    puts("SDL_GetTicks");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_GetWindowDisplayIndex(SDL_Window * window)
 {
-    put("SDL_GetWindowDisplayIndex");
+    puts("SDL_GetWindowDisplayIndex");
     /* TODO */
     return 0;
 }
 
 Uint32 SDLCALL SDL_GetWindowFlags(SDL_Window * window)
 {
-    put("SDL_GetWindowFlags");
+    puts("SDL_GetWindowFlags");
     /* TODO */
     return 0;
 }
 
 Uint32 SDLCALL SDL_GetWindowID(SDL_Window * window)
 {
-    put("SDL_GetWindowID");
+    puts("SDL_GetWindowID");
     /* TODO */
     return 0;
 }
 
 Uint32 SDLCALL SDL_GetWindowPixelFormat(SDL_Window * window)
 {
-    put("SDL_GetWindowPixelFormat");
+    puts("SDL_GetWindowPixelFormat");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_GetWindowSize(SDL_Window * window, int *w, int *h)
 {
-    put("SDL_GetWindowSize");
+    puts("SDL_GetWindowSize");
     /* TODO */
     return;
 }
 
 SDL_Surface * SDLCALL SDL_GetWindowSurface(SDL_Window * window)
 {
-    put("SDL_GetWindowSurface");
+    puts("SDL_GetWindowSurface");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_Init(Uint32 flags)
 {
-    put("SDL_Init");
+    puts("SDL_Init");
     /* TODO */
     return 0;
 }
 
 SDL_bool SDLCALL SDL_IsTextInputActive(void)
 {
-    put("SDL_IsTextInputActive");
+    puts("SDL_IsTextInputActive");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_JoystickClose(SDL_Joystick * joystick)
 {
-    put("SDL_JoystickClose");
+    puts("SDL_JoystickClose");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_JoystickEventState(int state)
 {
-    put("SDL_JoystickEventState");
+    puts("SDL_JoystickEventState");
     /* TODO */
     return 0;
 }
 
 Sint16 SDLCALL SDL_JoystickGetAxis(SDL_Joystick * joystick, int axis)
 {
-    put("SDL_JoystickGetAxis");
+    puts("SDL_JoystickGetAxis");
     /* TODO */
     return 0;
 }
 
 Uint8 SDLCALL SDL_JoystickGetButton(SDL_Joystick * joystick, int button)
 {
-    put("SDL_JoystickGetButton");
+    puts("SDL_JoystickGetButton");
     /* TODO */
     return 0;
 }
 
 SDL_JoystickGUID SDLCALL SDL_JoystickGetDeviceGUID(int device_index)
 {
-    put("SDL_JoystickGetDeviceGUID");
+    puts("SDL_JoystickGetDeviceGUID");
     /* TODO */
     SDL_JoystickGUID joystickguid;
     return joystickguid;
@@ -332,7 +332,7 @@ SDL_JoystickGUID SDLCALL SDL_JoystickGetDeviceGUID(int device_index)
 
 SDL_JoystickGUID SDLCALL SDL_JoystickGetGUIDFromString(const char *pchGUID)
 {
-    put("SDL_JoystickGetGUIDFromString");
+    puts("SDL_JoystickGetGUIDFromString");
     /* TODO */
     SDL_JoystickGUID joystickguid;
     return joystickguid;
@@ -340,322 +340,322 @@ SDL_JoystickGUID SDLCALL SDL_JoystickGetGUIDFromString(const char *pchGUID)
 
 Uint8 SDLCALL SDL_JoystickGetHat(SDL_Joystick * joystick, int hat)
 {
-    put("SDL_JoystickGetHat");
+    puts("SDL_JoystickGetHat");
     /* TODO */
     return 0;
 }
 
 const char *SDLCALL SDL_JoystickName(SDL_Joystick * joystick)
 {
-    put("SDL_JoystickName");
+    puts("SDL_JoystickName");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_JoystickNumAxes(SDL_Joystick * joystick)
 {
-    put("SDL_JoystickNumAxes");
+    puts("SDL_JoystickNumAxes");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_JoystickNumHats(SDL_Joystick * joystick)
 {
-    put("SDL_JoystickNumHats");
+    puts("SDL_JoystickNumHats");
     /* TODO */
     return 0;
 }
 
 SDL_Joystick *SDLCALL SDL_JoystickOpen(int device_index)
 {
-    put("SDL_JoystickOpen");
+    puts("SDL_JoystickOpen");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_LockAudio(void)
 {
-    put("SDL_LockAudio");
+    puts("SDL_LockAudio");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_LockMutex(SDL_mutex * mutex)
 {
-    put("SDL_LockMutex");
+    puts("SDL_LockMutex");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_LockSurface(SDL_Surface * surface)
 {
-    put("SDL_LockSurface");
+    puts("SDL_LockSurface");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_LowerBlit(SDL_Surface * src, SDL_Rect * srcrect, SDL_Surface * dst, SDL_Rect * dstrect)
 {
-    put("SDL_LowerBlit");
+    puts("SDL_LowerBlit");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_MixAudioFormat(Uint8 * dst, const Uint8 * src, SDL_AudioFormat format, Uint32 len, int volume)
 {
-    put("SDL_MixAudioFormat");
+    puts("SDL_MixAudioFormat");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_NumJoysticks(void)
 {
-    put("SDL_NumJoysticks");
+    puts("SDL_NumJoysticks");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_PauseAudio(int pause_on)
 {
-    put("SDL_PauseAudio");
+    puts("SDL_PauseAudio");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_PeepEvents(SDL_Event * events, int numevents, SDL_eventaction action, Uint32 minType, Uint32 maxType)
 {
-    put("SDL_PeepEvents");
+    puts("SDL_PeepEvents");
     /* TODO */
     return 0;
 }
 
 SDL_bool SDLCALL SDL_PixelFormatEnumToMasks(Uint32 format, int *bpp, Uint32 * Rmask, Uint32 * Gmask, Uint32 * Bmask, Uint32 * Amask)
 {
-    put("SDL_PixelFormatEnumToMasks");
+    puts("SDL_PixelFormatEnumToMasks");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_PollEvent(SDL_Event * event)
 {
-    put("SDL_PollEvent");
+    puts("SDL_PollEvent");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_PumpEvents(void)
 {
-    put("SDL_PumpEvents");
+    puts("SDL_PumpEvents");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_Quit(void)
 {
-    put("SDL_Quit");
+    puts("SDL_Quit");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_QuitSubSystem(Uint32 flags)
 {
-    put("SDL_QuitSubSystem");
+    puts("SDL_QuitSubSystem");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_RenderClear(SDL_Renderer * renderer)
 {
-    put("SDL_RenderClear");
+    puts("SDL_RenderClear");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_RenderCopy(SDL_Renderer * renderer, SDL_Texture * texture, const SDL_Rect * srcrect, const SDL_Rect * dstrect)
 {
-    put("SDL_RenderCopy");
+    puts("SDL_RenderCopy");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_RenderPresent(SDL_Renderer * renderer)
 {
-    put("SDL_RenderPresent");
+    puts("SDL_RenderPresent");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_RenderSetIntegerScale(SDL_Renderer * renderer, SDL_bool enable)
 {
-    put("SDL_RenderSetIntegerScale");
+    puts("SDL_RenderSetIntegerScale");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_RenderSetLogicalSize(SDL_Renderer * renderer, int w, int h)
 {
-    put("SDL_RenderSetLogicalSize");
+    puts("SDL_RenderSetLogicalSize");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_SetPaletteColors(SDL_Palette * palette, const SDL_Color * colors, int firstcolor, int ncolors)
 {
-    put("SDL_SetPaletteColors");
+    puts("SDL_SetPaletteColors");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_SetRelativeMouseMode(SDL_bool enabled)
 {
-    put("SDL_SetRelativeMouseMode");
+    puts("SDL_SetRelativeMouseMode");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_SetRenderDrawColor(SDL_Renderer * renderer, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
 {
-    put("SDL_SetRenderDrawColor");
+    puts("SDL_SetRenderDrawColor");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_SetRenderTarget(SDL_Renderer *renderer, SDL_Texture *texture)
 {
-    put("SDL_SetRenderTarget");
+    puts("SDL_SetRenderTarget");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_SetWindowFullscreen(SDL_Window * window, Uint32 flags)
 {
-    put("SDL_SetWindowFullscreen");
+    puts("SDL_SetWindowFullscreen");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_SetWindowIcon(SDL_Window * window, SDL_Surface * icon)
 {
-    put("SDL_SetWindowIcon");
+    puts("SDL_SetWindowIcon");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_SetWindowMinimumSize(SDL_Window * window, int min_w, int min_h)
 {
-    put("SDL_SetWindowMinimumSize");
+    puts("SDL_SetWindowMinimumSize");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_SetWindowSize(SDL_Window * window, int w, int h)
 {
-    put("SDL_SetWindowSize");
+    puts("SDL_SetWindowSize");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_SetWindowTitle(SDL_Window * window, const char *title)
 {
-    put("SDL_SetWindowTitle");
+    puts("SDL_SetWindowTitle");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_ShowSimpleMessageBox(Uint32 flags, const char *title, const char *message, SDL_Window *window)
 {
-    put("SDL_ShowSimpleMessageBox");
+    puts("SDL_ShowSimpleMessageBox");
     /* TODO */
     return 0;
 }
 
-void SDLCALL SDL_StartTextInput(void)
+void SDLCALL SDL_StartTextInputs(void)
 {
-    put("SDL_StartTextInput");
+    puts("SDL_StartTextInput");
     /* TODO */
     return;
 }
 
-void SDLCALL SDL_StopTextInput(void)
+void SDLCALL SDL_StopTextInputs(void)
 {
-    put("SDL_StopTextInput");
+    puts("SDL_StopTextInput");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_UnlockAudio(void)
 {
-    put("SDL_UnlockAudio");
+    puts("SDL_UnlockAudio");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_UnlockMutex(SDL_mutex * mutex)
 {
-    put("SDL_UnlockMutex");
+    puts("SDL_UnlockMutex");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_UnlockSurface(SDL_Surface * surface)
 {
-    put("SDL_UnlockSurface");
+    puts("SDL_UnlockSurface");
     /* TODO */
     return;
 }
 
 int SDLCALL SDL_UpdateTexture(SDL_Texture * texture, const SDL_Rect * rect, const void *pixels, int pitch)
 {
-    put("SDL_UpdateTexture");
+    puts("SDL_UpdateTexture");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_UpdateWindowSurface(SDL_Window * window)
 {
-    put("SDL_UpdateWindowSurface");
+    puts("SDL_UpdateWindowSurface");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_UpdateWindowSurfaceRects(SDL_Window * window, const SDL_Rect * rects, int numrects)
 {
-    put("SDL_UpdateWindowSurfaceRects");
+    puts("SDL_UpdateWindowSurfaceRects");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_UpperBlit(SDL_Surface * src, const SDL_Rect * srcrect, SDL_Surface * dst, SDL_Rect * dstrect)
 {
-    put("SDL_UpperBlit");
+    puts("SDL_UpperBlit");
     /* TODO */
     return 0;
 }
 
 int SDLCALL SDL_WaitEvent(SDL_Event * event)
 {
-    put("SDL_WaitEvent");
+    puts("SDL_WaitEvent");
     /* TODO */
     return 0;
 }
 
 void SDLCALL SDL_WaitThread(SDL_Thread * thread, int *status)
 {
-    put("SDL_WaitThread");
+    puts("SDL_WaitThread");
     /* TODO */
     return;
 }
 
 void SDLCALL SDL_WarpMouseInWindow(SDL_Window * window, int x, int y)
 {
-    put("SDL_WarpMouseInWindow");
+    puts("SDL_WarpMouseInWindow");
     /* TODO */
     return;
 }
 
 SDL_bool SDLCALL SDL_SetHint(const char *name, const char *value)
 {
-    put("SDL_SetHint");
+    puts("SDL_SetHint");
     /* TODO */
     return 0;
 }
--- doomodroidgo/main/libodroidSDL2Mixer.c
+++ doomodroidgo/main/libodroidSDL2Mixer.c
@@ -2,154 +2,154 @@
 
 int SDLCALL Mix_HaltMusic(void)
 {
-    put("Mix_HaltMusic");
+    puts("Mix_HaltMusic");
     /* TODO */
     return 0;
 }
 
 void SDLCALL Mix_CloseAudio(void)
 {
-    put("Mix_CloseAudio");
+    puts("Mix_CloseAudio");
     /* TODO */
     return;
 }
 
 int SDLCALL Mix_QuerySpec(int *frequency,Uint16 *format,int *channels)
 {
-    put("Mix_QuerySpec");
+    puts("Mix_QuerySpec");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_OpenAudio(int frequency, Uint16 format, int channels, int chunksize)
 {
-    put("Mix_OpenAudio");
+    puts("Mix_OpenAudio");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_Init(int flags)
 {
-    put("Mix_Init");
+    puts("Mix_Init");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_RegisterEffect(int chan, Mix_EffectFunc_t f, Mix_EffectDone_t d, void *arg)
 {
-    put("Mix_RegisterEffect");
+    puts("Mix_RegisterEffect");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_VolumeMusic(int volume)
 {
-    put("Mix_VolumeMusic");
+    puts("Mix_VolumeMusic");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_PlayMusic(Mix_Music *music, int loops)
 {
-    put("Mix_PlayMusic");
+    puts("Mix_PlayMusic");
     /* TODO */
     return 0;
 }
 
 void SDLCALL Mix_PauseMusic(void)
 {
-    put("Mix_PauseMusic");
+    puts("Mix_PauseMusic");
     /* TODO */
     return;
 }
 
 void SDLCALL Mix_ResumeMusic(void)
 {
-    put("Mix_ResumeMusic");
+    puts("Mix_ResumeMusic");
     /* TODO */
     return;
 }
 
 void SDLCALL Mix_FreeMusic(Mix_Music *music)
 {
-    put("Mix_FreeMusic");
+    puts("Mix_FreeMusic");
     /* TODO */
     return;
 }
 
 Mix_Music * SDLCALL Mix_LoadMUS(const char *file)
 {
-    put("Mix_LoadMUS");
+    puts("Mix_LoadMUS");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_PlayingMusic(void)
 {
-    put("Mix_PlayingMusic");
+    puts("Mix_PlayingMusic");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_SetMusicPosition(double position)
 {
-    put("Mix_SetMusicPosition");
+    puts("Mix_SetMusicPosition");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_SetMusicCMD(const char *command)
 {
-    put("Mix_SetMusicCMD");
+    puts("Mix_SetMusicCMD");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_HaltChannel(int channel)
 {
-    put("Mix_HaltChannel");
+    puts("Mix_HaltChannel");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_SetPanning(int channel, Uint8 left, Uint8 right)
 {
-    put("Mix_SetPanning");
+    puts("Mix_SetPanning");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_PlayChannelTimed(int channel, Mix_Chunk *chunk, int loops, int ticks)
 {
-    put("Mix_PlayChannelTimed");
+    puts("Mix_PlayChannelTimed");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_Playing(int channel)
 {
-    put("Mix_Playing");
+    puts("Mix_Playing");
     /* TODO */
     return 0;
 }
 
 int SDLCALL Mix_AllocateChannels(int numchans)
 {
-    put("Mix_AllocateChannels");
+    puts("Mix_AllocateChannels");
     /* TODO */
     return 0;
 }
 
 void SDLCALL Mix_HookMusic(void (SDLCALL *mix_func)(void *udata, Uint8 *stream, int len), void *arg)
 {
-    put("Mix_HookMusic");
+    puts("Mix_HookMusic");
     /* TODO */
     return;
 }
 
 void SDLCALL Mix_SetPostMix(void (SDLCALL *mix_func)(void *udata, Uint8 *stream, int len), void *arg)
 {
-    put("Mix_SetPostMix");
+    puts("Mix_SetPostMix");
     /* TODO */
     return;
 }
