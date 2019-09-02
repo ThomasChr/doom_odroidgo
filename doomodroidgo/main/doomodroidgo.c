d57b96cc451efd8861d80372729951d15065af15
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-09-02 13:04:32 +0200

    Change task stacksize and pin task to core 0

--- doomodroidgo/main/doomodroidgo.c
+++ doomodroidgo/main/doomodroidgo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include "sdkconfig.h"
 
 /* Source is from i_main.c */
@@ -11,20 +10,6 @@
 #include "doomtype.h"
 #include "i_system.h"
 #include "m_argv.h"
-
-void blinkmain(void *pvParameter)
-{
-    // make LED Output
-    gpio_pad_select_gpio(2);
-    gpio_set_direction(2, GPIO_MODE_OUTPUT);
-
-    while (1) {
-        gpio_set_level(2, 0);
-        vTaskDelay(50 / portTICK_PERIOD_MS);
-        gpio_set_level(2, 1);
-        vTaskDelay(50 / portTICK_PERIOD_MS);
-    }
-}
 
 void odroiddoommain()
 {
@@ -49,10 +34,7 @@ void odroiddoommain()
 
 void app_main()
 {
-    // Blinktask
-    xTaskCreate(&blinkmain, "doom_blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
-
-    // Doomtask -> Stacksize of 512 Words - which means 2048 Bytes
-    xTaskCreate(&odroiddoommain, "doom_main_task", 512, NULL, 5, NULL);
+    // Doomtask -> Stacksize of 25600 Bytes (25 KB), pinned to core 0
+    xTaskCreatePinnedToCore(&odroiddoommain, "doom_main_task", 22480, NULL, 5, NULL, 0);
 }
 
