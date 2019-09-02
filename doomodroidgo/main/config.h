597a988ae7e68ba912a1dc858f9884a297634497
Thomas Christlieb <thomaschristlieb@hotmail.com>
2019-09-02 11:35:30 +0200

    Change packagename

--- doomodroidgo/main/config.h
+++ doomodroidgo/main/config.h
@@ -41,15 +41,15 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 /* Name of package */
-#define PACKAGE "chocolate-doom"
+#define PACKAGE "chocolate-doom-odroidgo"
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "chocolate-doom-dev-list@chocolate-doom.org"
+#define PACKAGE_BUGREPORT "thomaschristlieb@hotmail.com"
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "Chocolate Doom"
+#define PACKAGE_NAME "Chocolate Doom Odroid Go"
 /* Define to the full name and version of this package. */
 #define PACKAGE_STRING "Chocolate Doom 3.0.0"
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "chocolate-doom"
+#define PACKAGE_TARNAME "chocolate-doom-odroigdo"
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 /* Define to the version of this package. */
